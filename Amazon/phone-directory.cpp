e6bfc47231886afd257d35527cd1b5f94b5ee2db
Charan Vaddepally <charan316@Charans-BacMook-Air.local>
2022-01-10 20:18:54 +0530

    AZ q5 completed

---
+++ Amazon/phone-directory.cpp
@@ -0,0 +1,66 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<vector<string>> displayContacts(int n, string contact[], string s)
+    {
+        // code here
+        
+        vector<vector<string>> ans;
+        bool check[n];
+        for(int i=0; i<n; i++){
+            check[i] = true;
+        }
+        for(int j=0; j<s.length(); j++){
+            bool found = false;
+            vector<string> st;
+            for(int i=0; i<n; i++){
+                if(contact[i][j]==s[j] && check[i]){
+                    st.push_back(contact[i]);
+                    found = true;
+                    check[i] = true;
+                }
+                else{
+                    check[i] = false;
+                }
+            }
+            if(found){
+                ans.push_back(st);
+            }
+            else{
+                ans.push_back({{"0"}});
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string contact[n], s;
+        for(int i = 0;i < n;i++)
+            cin>>contact[i];
+        cin>>s;
+        
+        Solution ob;
+        vector<vector<string>> ans = ob.displayContacts(n, contact, s);
+        for(int i = 0;i < s.size();i++){
+            for(auto u: ans[i])
+                cout<<u<<" ";
+            cout<<"\n";
+        }
+    }
+    return 0;
+} 
