bd5005afbcd1c65e15f648599837e558b7515538
Charan Vaddepally <charan316@Charans-BacMook-Air.local>
2022-01-10 19:44:11 +0530

    AZ q6 completed

---
+++ Amazon/max-subarray-sums-k.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<algorithm>
+using namespace std;
+
+vector<int> maxOfSubArrays(int arr[], int n, int k){
+    vector<int> ans;
+    multiset<int, greater<int> > window;
+    for(int i=0; i<k; i++){
+        window.insert(arr[i]);
+    }
+    ans.push_back(*window.begin());
+    for(int i=k; i<n; i++){
+        window.insert(arr[i]);
+        auto it = window.find(arr[i-k]);
+        window.erase(it);
+        ans.push_back(*window.begin());
+    }
+    return ans;
+}
+
+int main(){
+    int n,k;
+    cin >> n >> k;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    vector<int> ans = maxOfSubArrays(arr,n,k);
+    for(auto it: ans){
+        cout << it << " ";
+    }
+    cout << endl;
+    return 0;
+}
