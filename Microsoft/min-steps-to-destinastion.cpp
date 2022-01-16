6b66cf3076f382ccba1a7b2ca418ba7035130dc2
Charan Vaddepally <charan316@Charans-BacMook-Air.local>
2022-01-16 15:06:27 +0530

    MS q14 completed

--- Microsoft/min-steps-to-destinastion.cpp
+++ Microsoft/min-steps-to-destinastion.cpp
@@ -4,11 +4,7 @@ using namespace std;
 int minSteps(int D){
     int steps = 0;
     int sum = 0;
-    while(sum<D) {
-        sum+=steps;
-        steps++;
-    }
-    while((sum-D)%2) {
+    while(sum<D || (sum>D && (sum-D)%2)) {
         sum+=steps;
         steps++;
     }
